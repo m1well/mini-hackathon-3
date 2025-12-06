d8a1437769f30276011077af95ed754333a24710
cyborg-s <nysascha1@gmail.com>
2025-12-06 10:28:07 +0100

    job update

--- mh3-frontend/src/app/core/services/joboffer.service.ts
+++ mh3-frontend/src/app/core/services/joboffer.service.ts
@@ -18,13 +18,15 @@ export class JobOfferService {
 
   async updateOffer(userCode: string, job: JobOffer) {
     const payload = {
-      uniqueKey: job.uniqueKey,
-      urlJob: job.urlJob,
-      urlCompany: job.urlCompany,
-      urlCompanyLogo: job.urlCompanyLogo,
-      urlKununu: job.urlKununu,
-      urlLinkedin: job.urlLinkedin
-    };
+  uniqueKey: job.uniqueKey,
+  status: job.status, // <-- hier hinzufügen
+  urlJob: job.urlJob,
+  urlCompany: job.urlCompany,
+  urlCompanyLogo: job.urlCompanyLogo,
+  urlKununu: job.urlKununu,
+  urlLinkedin: job.urlLinkedin
+};
+
 
     const res = await fetch(`${ environment.apiUrl }/job/${ userCode }`, {
       method: 'PUT',
